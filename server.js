eb8db92357410dd9898959d825759bfd094f86d2
anantsharma108 <anantwwwww@gmail.com>
2025-07-23 10:00:01 +0530

    testing login page

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express=require('express')
 const app=express()
 
+app.set('view engine','ejs') 
+app.use(express.static('public')) 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 const PORT =process.env.PORT ||3000;
 
 app.get('/',(req,res)=>{
-    res.send('Anant')
+    res.send("Anant Sharma's render server")
 })
 
 app.get('/login',(req,res)=>{
-    res.send('Anant login page')
+    res.render('login')
 })
 
+app.post('/authenticate',(req,res)=>{
+    const {username,password}=req.body
+    if(username===process.env.USERNAME && password===process.env.PASSWORD){
+        res.send('user authenticated successfully')
+    }else{
+        res.send('credentials not match')
+    }
+
+})
 
 app.listen(PORT)
