2065f498c0866fc18ab78e8702425f71666b3f2c
anantsharma108 <anantwwwww@gmail.com>
2025-07-23 07:51:04 +0530

    adding login route

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express=require('express')
 const app=express()
 
 const PORT =process.env.PORT ||3000;
+
 app.get('/',(req,res)=>{
     res.send('Anant')
 })
+
+app.get('/login',(req,res)=>{
+    res.send('Anant login page')
+})
+
 
 app.listen(PORT)
